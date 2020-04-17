3a7e8797e5ec770133a38884cb79a3a1d67cba87
Sunny Lin <slin@csusb.edu>
2020-04-17 12:55:41 -0700

    Add examples codes for week 4

---
+++ week4/ccnumber.cpp
@@ -0,0 +1,24 @@
+// ccnumber.cpp
+// Removes all spaces or dashes from a string 
+#include <iostream>
+#include <string>
+using namespace std; 
+int main() 
+{ 
+	string credit_card_number = "4123-5678-9012-3450"; 
+	int i = 0; 
+	while (i < credit_card_number.length()) 
+	{ 	string ch = credit_card_number.substr(i, 1); 
+		if (ch == " " || ch == "-") //must remove char
+ 		{ 
+			string before = credit_card_number.substr(0, i); 
+			string after = credit_card_number.substr(i + 1);
+			credit_card_number = before + after; 
+		} 
+		else // no need to remove it, go to next char
+		{ i++; } 
+	} 
+	cout << credit_card_number << endl; 
+	return 0; 
+}
+
